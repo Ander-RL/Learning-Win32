d62728040d5584944c3de3ca97631e293fe945e4
Ander-RL <anderrodriguez205@gmail.com>
2021-08-14 11:40:34 +0200

    Adding unstaged files (Git).

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -28,6 +28,6 @@ public:
 
 	virtual PCWSTR  GetClassName() const;
 
-	virtual LRESULT HandleMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 };
